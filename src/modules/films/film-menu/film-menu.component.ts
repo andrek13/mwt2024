2d43ef9c4539ef0506603d13cd6ab3bb67fcdd3f
apopovych <andrii.popovych@student.upjs.sk>
2024-07-08 00:48:08 +0200

    module create

---
+++ src/modules/films/film-menu/film-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-film-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './film-menu.component.html',
+  styleUrl: './film-menu.component.css'
+})
+export class FilmMenuComponent {
+
+}
